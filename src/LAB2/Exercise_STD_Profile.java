00c4766bbe893426267e43881cf01cbf694caa0d
Windows 10 <kiwmuk456@gmail.com>
2021-01-10 16:34:22 +0700

    ex

--- src/LAB2/Exercise_STD_Profile.java
+++ src/LAB2/Exercise_STD_Profile.java
@@ -3,7 +3,6 @@ package LAB2;
 import java.util.Scanner;
 
 public class Exercise_STD_Profile {
-
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
---
+++ src/LAB3/ex_for.java
@@ -0,0 +1,15 @@
+package LAB3;
+
+public class ex_for {
+    public static void main(String[] args) {
+        //1.พิมข้อความ Hello 10ครั้ง
+        for (int i = 0; i <10 ; i++)
+            System.out.println("Hello "+i);
+            System.out.println("MIT ");
+
+        System.out.println("Good Bye");
+
+
+
+    }//
+}//
